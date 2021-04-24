4faa3140620fc76f648a9d5ff6baceabfab03d6c
wy1996-hub <1728587757@qq.com>
2021-04-24 21:25:51 +0800

    上传基于opencv/PIL的深度学习图像与处理增强代码（方法并不全，只有部分，后期可能会继续更新）

---
+++ paddle_image_augment/image_augment.py
@@ -0,0 +1,80 @@
+# 数据预处理
+# 在计算机视觉中，通常会对图像做一些随机的变化，产生相似但又不完全相同的样本。
+# 主要作用是扩大训练数据集，抑制过拟合，提升模型的泛化能力，常用的方法见下面的程序。
+
+import numpy as np
+import random
+import cv2
+from PIL import Image, ImageEnhance
+
+# 1.随机改变亮暗、对比度和颜色等
+def random_distort(img):
+    # 随机改变亮度
+    def random_brightness(img, lower=0.5, upper=1.5):
+        e = np.random.uniform(lower, upper)
+        return ImageEnhance.Brightness(img).enhance(e)
+    # 随机改变对比度
+    def random_contrast(img, lower=0.5, upper=1.5):
+        e = np.random.uniform(lower, upper)
+        return ImageEnhance.Contrast(img).enhance(e)
+    # 随机改变颜色
+    def random_color(img, lower=0.5, upper=1.5):
+        e = np.random.uniform(lower, upper)
+        return ImageEnhance.Color(img).enhance(e)
+
+    ops = [random_brightness, random_contrast, random_color]
+    np.random.shuffle(ops)
+    img = Image.fromarray(img)
+    img = ops[0](img)
+    img = ops[1](img)
+    img = ops[2](img)
+    img = np.asarray(img)
+
+    return img
+
+# 2.随机填充
+def random_expand(img, gtboxes, max_ratio=4, fill=None, keep_ratio=True, thresh=0.5):
+    if random.random() < 1.0:
+        return img, gtboxes
+    if max_ratio < 1.0:
+        return img, gtboxes
+    h, w, c = img.shape
+    ratio_x = random.uniform(1, max_ratio)
+    if keep_ratio:
+        ratio_y = ratio_x
+    else:
+        ratio_y = random.uniform(1, max_ratio)
+
+    oh = int(h * ratio_y)
+    ow = int(w * ratio_x)
+    off_x = random.randint(0, ow - w)
+    off_y = random.randint(0, oh - h)
+
+    out_img = np.zeros((oh, ow, c))
+    if fill and len(fill)==c:
+        for i in range(c):
+            out_img[:, :, i] = fill[i]*255.0
+
+    out_img[off_y:off_y + h, off_x:off_x + w, :] = img
+    gtboxes[:, 0] = ((gtboxes[:, 0] * w) + off_x) / float(ow)
+    gtboxes[:, 1] = ((gtboxes[:, 1] * h) + off_y) / float(oh)
+    gtboxes[:, 2] = gtboxes[:, 2] / ratio_x
+    gtboxes[:, 3] = gtboxes[:, 3] / ratio_y
+
+    return out_img.astype('uint8'), gtboxes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
